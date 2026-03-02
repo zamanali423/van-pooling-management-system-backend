46ed8c09481f59830c82101ce27c511d40877c41
Zaman Ali <zamanyaseen2024@gmail.com>
2026-03-02 20:25:52 +0500

    update

--- controllers/police/policeController.js
+++ controllers/police/policeController.js
@@ -3,6 +3,15 @@ const { pool } = require("../../utils/dbConnection");
 driverApplications = async (req, res) => {
   try {
     await pool.query("BEGIN");
+    const policeId = req.user.id;
+    const { rows: police } = await pool.query(
+      "SELECT * FROM users WHERE id=$1 AND role='POLICE'",
+      [policeId],
+    );
+    if (police.length === 0) {
+      await pool.query("ROLLBACK");
+      return res.status(403).json({ message: "Access denied" });
+    }
     const r = await pool.query(
       `
       SELECT 
@@ -11,20 +20,37 @@ driverApplications = async (req, res) => {
     U.email,
     U.phone,
     U.created_at,
-    CASE WHEN DA.is_approved = false THEN 'PENDING' ELSE 'VERIFIED' END AS status,
-    ARRAY_AGG(DISTINCT V.number_plate) AS vans,
+    DA.approved_at AS approval_date,
+    CASE 
+        WHEN DA.is_approved = true THEN 'VERIFIED'
+        WHEN DA.is_approved = false THEN 'REJECTED'
+        ELSE 'PENDING'
+        END AS status,
+     ARRAY_AGG(DISTINCT V.number_plate) 
+          FILTER (WHERE V.id IS NOT NULL) AS vans,
     JSONB_AGG(DISTINCT DD) FILTER (WHERE DD.id IS NOT NULL) AS driver_documents
-FROM users U
-LEFT JOIN driver_approvals DA ON U.id = DA.driver_id
-LEFT JOIN driver_documents DD ON U.id = DD.driver_id
-LEFT JOIN vans V ON U.id = V.driver_id
-WHERE U.role = 'DRIVER' 
-  AND U.is_verified = true
-GROUP BY U.id,DA.is_approved;
+    FROM users U
+    LEFT JOIN driver_approvals DA ON U.id = DA.driver_id
+    LEFT JOIN driver_documents DD ON U.id = DD.driver_id
+    LEFT JOIN vans V ON U.id = V.driver_id
+    WHERE U.role = 'DRIVER' 
+      AND U.is_verified = true
+    GROUP BY 
+        U.id,
+        U.full_name,
+        U.email,
+        U.phone,
+        U.created_at,
+        DA.is_approved,
+        DA.approved_at;
     `,
     );
     await pool.query("COMMIT");
-    res.json(r.rows);
+    const drivers = {
+      drivers: r.rows,
+      police: police[0],
+    };
+    res.json(drivers);
   } catch (e) {
     await pool.query("ROLLBACK");
     res.status(500).json({ error: e.message });
@@ -45,11 +71,37 @@ verifyDriver = async (req, res) => {
       [is_approved, driver_id],
     );
     await pool.query("COMMIT");
-    res.json({ message: "Driver verified successfully" });
+    res.json({
+      message: `Driver ${is_approved ? "approved" : "rejected"} successfully`,
+    });
   } catch (e) {
     await pool.query("ROLLBACK");
     res.status(500).json({ error: e.message });
   }
 };
 
-module.exports = { driverApplications, verifyDriver };
+report = async (req, res) => {
+  try {
+    await pool.query("BEGIN");
+    const report = await pool.query(
+      `
+      SELECT COUNT(DD.driver_license) AS total_drivers_licensed,
+      COUNT(DD.id_card) AS total_drivers_id_card, 
+      COUNT(DD.vehicle_docs) AS total_drivers_vehicle_docs,
+      COUNT(DD.vehicle_photo) AS total_drivers_vehicle_photo,
+      COUNT(DD.number_plate) AS total_drivers_number_plate,
+      COUNT(CASE WHEN DA.is_approved = true THEN 1 END) AS total_verified_drivers,
+      COUNT(CASE WHEN DA.is_approved = false THEN 1 END) AS total_rejected_drivers
+      from driver_documents DD
+      JOIN driver_approvals DA ON DA.driver_id=DD.driver_id
+      `,
+    );
+    await pool.query("COMMIT");
+    res.json(report.rows[0]);
+  } catch (e) {
+    await pool.query("ROLLBACK");
+    res.status(500).json({ error: e.message });
+  }
+};
+
+module.exports = { driverApplications, verifyDriver, report };
--- routes/police/policeRoutes.js
+++ routes/police/policeRoutes.js
@@ -4,9 +4,11 @@ const verifyToken = require("../../middlewares/verifyToken");
 const {
   driverApplications,
   verifyDriver,
+  report,
 } = require("../../controllers/police/policeController");
 
 router.get("/driver-applications", verifyToken, driverApplications);
 router.put("/verify-driver/:driver_id", verifyToken, verifyDriver);
+router.get("/report", verifyToken, report);
 
 module.exports = router;
